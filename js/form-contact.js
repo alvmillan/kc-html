58ba4a3a9929a16308d87e2a0cda1553dc96f263
ALVARO MILLAN <amillan@protonmail.com>
2018-12-23 23:40:03 +0100

    first commit

---
+++ js/form-contact.js
@@ -0,0 +1,82 @@
+export class FormContact {
+    constructor() {
+
+        this.oFormContact =  document.querySelector('#contact-form')
+        this.oInputName = document.querySelector('#nombre')
+        this.oInputEmail = document.querySelector('#email')
+        this.oInputPhone = document.querySelector('#phone')
+        this.oTextMessage = document.querySelector('#message')
+        this.oSelectSeleccion = document.querySelector('#selection')
+        this.oOtherSelectionInput = document.querySelector('#otherSelection')
+
+        this.oData = {
+            name: '',
+            email: '',
+            phone: '',
+            message: '',            
+            seleccion: '',
+            otherSelection: ''
+        };
+
+        this.oFormContact.addEventListener('submit', this.leerContact.bind(this));
+        this.oSelectSeleccion.addEventListener('change', this.shouldOtherSelectionInputBeVisible.bind(this));
+    }
+
+    leerContact(oE) {
+        oE.preventDefault();
+        console.log("test");
+        if (this.validar()) {
+            this.guardarDatos()
+        }
+    }
+
+    guardarDatos() {
+        this.oData = {
+            name:  this.oInputName.value,
+            email: this.oInputEmail.value ,
+            phone: this.oInputPhone.value,
+            message: this.oTextMessage.value,
+            seleccion: this.oSelectSeleccion.options[this.oSelectSeleccion.selectedIndex].value,
+            otherSelection: this.oOtherSelectionInput.value
+        }
+
+        let msg = document.querySelector('#succ_post');  
+        console.log(msg);
+        msg.classList.remove('hidden');
+        setTimeout(() => msg.classList.add('hidden'), 3000);
+    }
+
+    definirValidaciones() {
+        // this.oInputName.setCustomValidity('El nombre es obligatorio')
+        console.dir(this.oInputName.validity)
+        console.dir(this.oInputEmail.validity)
+    }
+
+    shouldOtherSelectionInputBeVisible(oE) {
+        oE.preventDefault();
+        if ( this.oSelectSeleccion.options[this.oSelectSeleccion.selectedIndex].value === 'other') {            
+            this.oOtherSelectionInput.parentElement.classList.remove('form-toggle');
+        } else {
+            this.oOtherSelectionInput.parentElement.classList.add('form-toggle');
+        }
+    }
+
+    validar() {
+        if (this.validateTextArea()) {
+            document.querySelector('#err_message').classList.add('hidden');
+            return true;
+        } else {
+            document.querySelector('#err_message').classList.remove('hidden');
+        }
+    }
+
+    validateTextArea() {
+        const maxWords = 150;
+        let getNumberOfWords = this.oTextMessage.value.split(' ').length;
+        if (getNumberOfWords > maxWords ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,25 @@
+import { FormContact} from './form-contact.js'
+import { Menu } from './menu.js'
+
+/**
+ * 
+ * @class Index
+ * 
+ * Se instancia al acceder a la página index
+ * Depende de:
+ *  - la clase Menu para gestionar los menus, 
+ *      comportidos con la otra página del sitio
+ *  - la clase FormCoctact, responsable del 
+ *      formulario de contactos
+ * 
+ */
+
+export class Index {
+    constructor() {
+        // elementos del DOM       
+        this.oInputName = document.querySelector('#nombre');
+        this.oMenus = new Menu();
+        this.oFormContact = new FormContact();
+    }
+}
+
